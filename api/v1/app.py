65315e5fdf9eb758fc2292df697677bf9ba01552
maqamylee0 <maqamylee0@gmail.com>
2024-02-23 16:02:27 +0300

    create api

---
+++ api/v1/app.py
@@ -0,0 +1,21 @@
+from models import storage
+from api.v1.views import app_views
+from flask import Flask, jsonify
+from os import environ
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({"error": "Not found"})
+
+if __name__ == '__main__':
+    host = environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = environ.get('HBNB_API_PORT', '5000')
+    app.run(host=host, port=port, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,6 @@
+from flask import jsonify
+
+def init_app(app_views):
+    @app_views.route('/status', methods=['GET'])
+    def status():
+        return jsonify({"status": "OK"})
