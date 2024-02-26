db09bf3059fce11704eb9ffc15272701d0ea3679
dideey <bradleyjd32@gmail.com>
2024-02-27 02:14:53 +0300

    error handling

--- api/v1/app.py
+++ api/v1/app.py
@@ -23,7 +23,7 @@ def teardown_db(exception):
 @app.errorhandler(404)
 def not_found(error):
     """404 page"""
-    return jsonify({"error": "Not found"})
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == '__main__':
